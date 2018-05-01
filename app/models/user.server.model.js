fbf3bde59e377aa349cbf8d94a49b307b1ab1b2d
ccampell <ccampell.asu@gmail.com>
2018-05-01 16:59:42 -0400

    Added a user.server.model.js file to house each user's SHA256 secret key which is used to verify the user once they click the magic auth link. TODO: Use salt to encrypt the user's secret key in the database.

---
+++ app/models/user.server.model.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+   email: {
+    type: String,
+    required: 'Email is required!',
+    trim: true
+  },
+  jwt-secret-key: {
+    //TODO: This should not be stored in plaintext, use salt to encrypt the SHA256 secret key.
+    type: Integer,
+    required: 'User\'s secret key is required for authentication!',
+  }
+});
+
+mongoose.model('User', UserSchema);
