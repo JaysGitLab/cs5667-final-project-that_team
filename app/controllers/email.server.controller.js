14662ec9bc712ffb3f8d245151904dc1e2206c95
ccampell <ccampell.asu@gmail.com>
2018-05-01 17:29:11 -0400

    Added function to email.server.controller.js which will hopefully generate a secret key for a user if they do not already exist in the database. Otherwise a message is printed to the console indicating that a secret key will not be regenerated.

---
+++ app/controllers/email.server.controller.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+const User = mongoose.model('User');
+/*
+* This file handles the sending of a magic authentication link via email which the user can click on to authenticate.
+*/
+
+const generate_user_secret_key = (email_address) {
+    if User.statics.userExists(email_address) {
+        console.log('Tried to generate a secret key for a user already in the database, key not regenerated.')
+    } else {
+        console.log('Requested a secret key for a user not already in the database, generating new secrete key.')
+        /*
+        *  I believe this method will create the secret key based off the user's email address and then store it in
+        *   the database using the UserSchema's jwt-secret-key field.
+        */
+        jwt.sign({ id: email_address.id }, User.jwt-secret-key)
+    }
+
+}
+
+module.exports = { generate_user_secret_key };
