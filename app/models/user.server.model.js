d786c0d099e21a88f57e83d263b65d163b0b1414
ccampell <ccampell.asu@gmail.com>
2018-05-02 08:15:01 -0400

    Added schema methods.

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
@@ -7,7 +8,7 @@ const UserSchema = new Schema({
     required: 'Email is required!',
     trim: true
   },
-  jwt-secret-key: {
+  jwtSecretKey: {
     //TODO: This should not be stored in plaintext, use salt to encrypt the SHA256 secret key.
     type: Number,
     required: 'User\'s secret key is required for authentication!',
@@ -19,7 +20,33 @@ const UserSchema = new Schema({
 * This function returns true if the provided email_address is already in the database.
 */
 UserSchema.statics.userExists = function(email_address) {
-    return this.find({email: new RegExp(email_address, 'i')})
+    return this.find({email: new RegExp(email_address, 'i')});
 }
+
+/*
+* This function is callable by UserSchema mongoose.model objects (see: http://mongoosejs.com/docs/guide.html#methods)
+* returns true if the subclass has a jwtSecretKey in the database already.
+*/
+UserSchema.methods.has_secret_key = function(err) {
+    if (err) {
+        console.log(err + 'occurred.');
+    } else {
+        return this.model('User').jwtSecretKey;
+    }
+}
+
+/*
+* This function is callable by User mongoose.model type subclasses to generate a new jwtSecretKey for this user in
+* the database.
+*/
+UserSchema.methods.generate_new_secret_key = function(err) {
+    if (err) {
+        console.log(err + 'occurred.');
+    } else {
+        this.model('User').jwtSecretKey = jwt.sign({ id: email_address.id }, this.model('User').jwt-secret-key);
+    }
+}
+
+
 
 mongoose.model('User', UserSchema);
