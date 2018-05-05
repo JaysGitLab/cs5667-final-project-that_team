2262851028f1b7470c95553a1dd66c6bdcdf1a79
ccampell <ccampell.asu@gmail.com>
2018-05-05 18:03:07 -0400

    Fixed spelling error, now server is logging received login attempts.

--- config/socketio.js
+++ config/socketio.js
@@ -2,6 +2,7 @@
 const config = require('./config');
 const cookieParser = require('cookie-parser');
 const configureReservation = require('../app/controllers/reservation.server.controller.js');
+const configureAdmin = require('../app/controllers/admin.server.controller.js');
 /* TODO: Add SocketIO component controllers here */
 
 // Define the Socket.io configuration method
@@ -9,6 +10,7 @@ module.exports = function(server, io) {
 	// Add an event listener to the 'connection' event
   io.on('connection', (socket) => {
       configureReservation(io, socket);
+      configureAdmin(io, socket);
       //TODO: add other socket configurations here.
   });
 };
