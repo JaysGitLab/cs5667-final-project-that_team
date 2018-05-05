c79e793c64fbee36d1342820bbd92136d176e740
ccampell <ccampell.asu@gmail.com>
2018-05-05 15:25:58 -0400

    Added AdminSchema to models.

---
+++ app/models/admin.server.model.js
@@ -0,0 +1,85 @@
+const mongoose = require('mongoose');
+const crypto = require('crypto');
+const Schema = mongoose.Schema;
+const AdminSchema = new Schema({
+    firstName: String,
+    lastName: String,
+    email: {
+        type: String,
+        match: [/.+\@.+\..+/, "Please fill a valid e-mail address"]
+    },
+    username: {
+        type: String,
+        unique: true,
+        required: 'Username is required',
+        trim: true
+    },
+    password: {
+        type: String,
+        validate: [(password) => {
+                return password && password.length > 6;
+        }, 'Password should be longer']
+    },
+    salt: {
+        type: String
+    },
+    provider: {
+        type: String,
+        required: 'Provider is required'
+    },
+    providerId: String,
+    providerData: {},
+    created: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+AdminSchema.virtual('fullName').get(function() {
+    return this.firstName + ' ' + this.lastName;
+}).set(function(fullName) {
+    const splitName = fullName.split(' ');
+    this.firstName = splitName[0] || '';
+    this.lastName = splitName[1] || '';
+});
+
+AdminSchema.pre('save', function(next) {
+    if (this.password) {
+        this.salt = new
+        Buffer(crypto.randomBytes(16).toString('base64'), 'base64');
+        this.password = this.hashPassword(this.password);
+    }
+    next();
+});
+
+AdminSchema.methods.hashPassword = function(password) {
+    return crypto.pbkdf2Sync(password, this.salt, 10000, 64).toString('base64');
+};
+
+AdminSchema.methods.authenticate = function(password) {
+    return this.password === this.hashPassword(password);
+};
+
+AdminSchema.statics.findUniqueUsername = function(username, suffix, callback) {
+    var possibleUsername = username + (suffix || '');
+    this.findOne({
+        username: possibleUsername
+    }, (err, user) => {
+        if (!err) {
+            if (!user) {
+                callback(possibleUsername);
+            } else {
+                return this.findUniqueUsername(username, (suffix || 0), 1, callback);
+            }
+        } else {
+            callback(null);
+        }
+    });
+};
+
+AdminSchema.set('toJSON', {
+    getters: true,
+    virtuals: true
+});
+
+mongoose.model('Admin', AdminSchema)
