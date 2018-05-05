a8316517b9f5c8078c49a7bea33fa5470d37671b
ccampell <ccampell.asu@gmail.com>
2018-05-05 15:10:37 -0400

    Changed non-existant User model to Admin.

--- config/strategies/local.js
+++ config/strategies/local.js
@@ -1,6 +1,6 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
-const User = require('mongoose').model('User');
+const User = require('mongoose').model('Admin');
 
 module.exports = function() {
     passport.use(new LocalStrategy((username, password, done) => {
