916721e5452713351ffaf80ada08df2b9969bfb4
ccampell <ccampell.asu@gmail.com>
2018-05-05 16:38:49 -0400

    Created admin.component.ts

---
+++ public/app/admin/admin.component.ts
@@ -0,0 +1,60 @@
+import { Component } from '@angular/core';
+import { NgForm, FormControl, Validators, FormGroupDirective } from '@angular/forms';
+import { AdminService } from './admin.service';
+import { ErrorStateMatcher } from '@angular/material/core';
+
+/* Error when invalid control is dirty, touched, or submitted.
+ * (see: https://material.angular.io/components/input/examples)
+*/
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
+
+/*
+* This component handles admin local authentication requests via angular form and passport.
+*/
+@Component({
+    selector: 'admin',
+    templateUrl: './app/admin/admin.template.html',
+    providers: [AdminService]
+})
+export class AdminComponent {
+    dateFilter: any;
+    usernameFormControl = new FormControl('', [
+        Validators.required
+        // TODO: If we want Validator for username it would require a custom validator.
+    ]);
+    matcher = MyErrorStateMatcher();
+
+    /*
+    * Constructor for AdminComponent
+    *
+    * initializes a AdminService instance in the _adminservice attribute.
+    */
+    constructor(private _adminservice: AdminService) {}
+
+    /*
+    * This overrides the ngOnInit function to add functionality.
+    *
+    * Initializes class fields and sets up necessary socket.io hook.
+    */
+    ngOnInit() {
+        this._adminservice.on('message', (message) => {
+            console.log(message);
+        });
+    }
+
+    /*
+    * This method handles login form submissions.
+    */
+    onSubmit(form: NgForm) {
+        if (form.valid && this.usernameFormControl.valid) {
+            var submission = JSON.parse(JSON.stringify(form.value));
+            submission.uname = this.usernameFormControl.value;
+            this._adminservice.emit('adminLoginRequested', {form: JSON.stringify(submission)});
+        }
+    }
+}
