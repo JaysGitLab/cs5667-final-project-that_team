200ab4a1822849f4d291eb01e3d5fae77a34ffaf
ccampell <ccampell.asu@gmail.com>
2018-05-05 15:34:16 -0400

    Removed blank line.

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ const app = require('./config/express')(db);
 // Configure Passport for the Admin login:
 const configurePassport = require('./config/passport')();
 
-
 app.listen(3000);
 module.exports = app; //Export the running application.
 console.log('Park-reserve running at http://localhost:3000/'); //TODO: edit to reflect the address of the server host.
